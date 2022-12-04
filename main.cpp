294e290f5978e99757ab597f078fd3e63e1b0c22
Imrul <imrulanwar836@gmail.com>
2022-12-04 15:57:21 +0600

    Dfs & Bfs

--- main.cpp
+++ main.cpp
@@ -1,6 +1,81 @@
-#include <iostream>
+#include <bits/stdc++.h>
+
+#define ll long long
+#define clr(x) memset(x, 0, sizeof(x))
+
 using namespace std;
+
+
+void dfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
+	// O (n log n)
+	// use vector<ll> adjList[n+1] for non negative nodes -> O (n)
+	stack<ll> stck;
+	stck.push(source);
+	discovered[source] = 1;
+	while (!stck.empty()) {
+		ll curr = stck.top();
+
+		cout << curr << " ";
+
+		stck.pop();
+		for (auto e: adjList[curr])
+			if (!discovered[e]) {
+				stck.push(e);
+				discovered[e] = 1;
+			}
+	}
+	cout << endl;
+	return;
+}
+
+void bfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
+	// O (n log n)
+	// use vector<ll> adjList[n+1] for non negative nodes -> O (n)
+	queue<ll> que;
+	que.push(source);
+	discovered[source] = 1;
+	while (!que.empty()) {
+		ll curr = que.front();
+
+		cout << curr << " ";
+
+		que.pop();
+		for (auto e: adjList[curr])
+			if (!discovered[e]) {
+				que.push(e);
+				discovered[e] = 1;
+			}
+	}
+	cout << endl;
+	return;
+}
+
 int main() {
+	ll n, e;
+	cin >> n >> e;
+	map<ll, vector<ll>> adjList;
+	while (e--) {
+		ll x, y;
+		cin >> x >> y;
+		adjList[x].push_back(y);
+	}
+	ll source = 1;
+	vector<ll> discovered(n + 1, 0);
+	bfs(adjList, source, discovered);
+	dfs(adjList, source, discovered);
 
 	return 0;
 }
+
+/*
+6 9
+1 3
+1 5
+3 5
+3 4
+3 6
+3 2
+2 6
+4 6
+5 6
+ */
