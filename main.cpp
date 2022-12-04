a70547f932b04dc20205cb34d6a62b4cb0badd42
Imrul <imrulanwar836@gmail.com>
2022-12-04 18:11:12 +0600

    Dsu

--- main.cpp
+++ main.cpp
@@ -1,22 +1,37 @@
 #include <bits/stdc++.h>
 
 #define ll long long
-#define clr(x) memset(x, 0, sizeof(x))
 
 using namespace std;
 
+// dsu
+void make(ll v, ll parent[]) {
+	// O(1)
+	parent[v] = v;
+}
+
+ll fnd(ll v, ll parent[]) {
+	// O(1)
+	if (parent[v] == v) return v;
+	return parent[v] = fnd(parent[v], parent);
+}
+
+void Union(ll a, ll b, ll parent[]) {
+	// O(1)
+	parent[fnd(a, parent)] = parent[fnd(b, parent)];
+}
+// dsu
 
 void dfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
 	// O (n log n)
 	// use vector<ll> adjList[n+1] for non negative nodes -> O (n)
+	// use pair<ll, ll> instead of ll for weighted graphs
 	stack<ll> stck;
 	stck.push(source);
 	discovered[source] = 1;
 	while (!stck.empty()) {
 		ll curr = stck.top();
-
 		cout << curr << " ";
-
 		stck.pop();
 		for (auto e: adjList[curr])
 			if (!discovered[e]) {
@@ -31,14 +46,13 @@ void dfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
 void bfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
 	// O (n log n)
 	// use vector<ll> adjList[n+1] for non negative nodes -> O (n)
+	// use pair<ll, ll> instead of ll for weighted graphs
 	queue<ll> que;
 	que.push(source);
 	discovered[source] = 1;
 	while (!que.empty()) {
 		ll curr = que.front();
-
 		cout << curr << " ";
-
 		que.pop();
 		for (auto e: adjList[curr])
 			if (!discovered[e]) {
@@ -54,6 +68,10 @@ int main() {
 	ll n, e;
 	cin >> n >> e;
 	map<ll, vector<ll>> adjList;
+	// graph list
+	// graph map
+	// weighted graph list
+	// weighted graph map
 	while (e--) {
 		ll x, y;
 		cin >> x >> y;
