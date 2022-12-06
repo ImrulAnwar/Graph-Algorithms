717e08dae5d4fb1dddcd595adb20c8dfb69f1368
Imrul <imrulanwar836@gmail.com>
2022-12-06 13:59:23 +0600

    Kruskal for minimum spanning tree

--- main.cpp
+++ main.cpp
@@ -5,22 +5,44 @@
 using namespace std;
 
 // dsu
-void make(ll v, ll parent[]) {
+void make(ll node, ll parent[]) {
 	// O(1)
-	parent[v] = v;
+	// creates a disconnected component
+	parent[node] = node;
 }
 
-ll fnd(ll v, ll parent[]) {
+ll fnd(ll node, ll parent[]) {
 	// O(1)
-	if (parent[v] == v) return v;
-	return parent[v] = fnd(parent[v], parent);
+	// returns the parent of a component
+	if (parent[node] == node) return node;
+	return parent[node] = fnd(parent[node], parent);
 }
 
 void Union(ll a, ll b, ll parent[]) {
 	// O(1)
+	// connects two components together
 	parent[fnd(a, parent)] = parent[fnd(b, parent)];
 }
 // dsu
+
+void kruskal(map<ll, pair<ll, ll>> &input, map<ll, pair<ll, ll>> &ans, ll N) {
+	// O(n log n)
+	// this will return the minimum spanning tree
+	ll parent[N + 1];
+	// making all the nodes
+	for (auto &e: input) {
+		make(e.second.first, parent);
+		make(e.second.second, parent);
+	}
+	for (auto &e: input) {
+		ll a = e.second.first;
+		ll b = e.second.second;
+		// if their parents are equal they will create a cycle
+		if (fnd(a, parent) == fnd(b, parent)) continue;
+		Union(a, b, parent);
+		ans[e.first] = {a, b};
+	}
+}
 
 void dfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
 	// O (n log n)
@@ -67,20 +89,24 @@ void bfs(map<ll, vector<ll>> &adjList, ll source, vector<ll> discovered) {
 int main() {
 	ll n, e;
 	cin >> n >> e;
-	map<ll, vector<ll>> adjList;
+	map<ll, pair<ll, ll>> input, ans;
 	// graph list
 	// graph map
 	// weighted graph list
 	// weighted graph map
 	while (e--) {
-		ll x, y;
-		cin >> x >> y;
-		adjList[x].push_back(y);
+		ll x, y, z;
+		cin >> x >> y >> z;
+		input[z] = {x, y};
 	}
-	ll source = 1;
-	vector<ll> discovered(n + 1, 0);
-	bfs(adjList, source, discovered);
-	dfs(adjList, source, discovered);
+	kruskal(input, ans, n);
+	for (auto &element: ans) {
+		cout << element.first << " " << element.second.first << " " << element.second.second << endl;
+	}
+//	ll source = 1;
+//	vector<ll> discovered(n + 1, 0);
+//	bfs(adjList, source, discovered);
+//	dfs(adjList, source, discovered);
 
 	return 0;
 }
@@ -96,4 +122,16 @@ int main() {
 2 6
 4 6
 5 6
+ */
+/*
+ 6 9
+ 5 4 9
+ 1 4 1
+ 5 1 4
+ 4 3 5
+ 4 2 3
+ 1 2 2
+ 3 2 3
+ 3 6 8
+ 2 6 7
  */
